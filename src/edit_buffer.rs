926e4c9c104a7aa48dcf182ec8177a68520645e0
Akira Hayakawa <ruby.wktk@gmail.com>
2019-03-04 17:40:09 +0900

    dev update
    Signed-off-by: Akira Hayakawa <ruby.wktk@gmail.com>

--- src/edit_buffer.rs
+++ src/edit_buffer.rs
@@ -30,6 +30,78 @@ struct ChangeLog {
     inserted: Vec<BufElem>,
 }
 
+pub struct ReadBuffer {
+    pub buf: Vec<Vec<BufElem>>,
+    pub cursor: Cursor,
+    num_buffer: Vec<char>,
+}
+
+impl ReadBuffer {
+    pub fn new() -> Self {
+        Self {
+            buf: vec![vec![]],
+            cursor: Cursor { row: 0, col: 0 },
+            num_buffer: vec![],
+        }
+    }
+    pub fn cursor_up(&mut self, _: Key) {
+        if self.cursor.row > 0 {
+            self.cursor.row -= 1;
+        }
+    }
+    pub fn cursor_down(&mut self, _: Key) {
+        if self.cursor.row < self.buf.len() - 1 {
+            self.cursor.row += 1;
+        }
+    }
+    pub fn cursor_left(&mut self, _: Key) {
+        if self.cursor.col > 0 {
+            self.cursor.col -= 1;
+        }
+    }
+    pub fn cursor_right(&mut self, _: Key) {
+        if self.cursor.col < self.buf[self.cursor.row].len() - 1 {
+            self.cursor.col += 1;
+        }
+    }
+    pub fn jump_line_head(&mut self, _: Key) {
+        self.cursor.col = 0;
+    }
+    pub fn jump_line_last(&mut self, _: Key) {
+        self.cursor.col = self.buf[self.cursor.row].len() - 1;
+    }
+    pub fn enter_jump_mode(&mut self, k: Key) {
+        self.num_buffer.clear();
+        match k {
+            Key::Char(c) => self.num_buffer.push(c),
+            _ => panic!(),
+        }
+    }
+    pub fn acc_jump_num(&mut self, k: Key) {
+        match k {
+            Key::Char(c) => self.num_buffer.push(c),
+            _ => panic!(),
+        }
+    }
+    pub fn jump(&mut self, _: Key) {
+        let mut s = String::new();
+        for c in self.num_buffer.clone() {
+            s.push(c);
+        }
+        let n = s.parse::<usize>().unwrap();
+        let row = n-1;
+        self.cursor.row = row;
+        self.cursor.col = 0;
+    }
+    pub fn cancel_jump(&mut self, _: Key) {
+        self.num_buffer.clear();
+    }
+    pub fn jump_last(&mut self, _: Key) {
+        self.cursor.row = self.buf.len() - 1;
+        self.cursor.col = 0;
+    }
+}
+
 pub struct EditBuffer {
     pub buf: Vec<Vec<BufElem>>,
     pub cursor: Cursor,
