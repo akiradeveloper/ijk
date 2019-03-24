a7e21debd79c69030d7662ecf5dfc5bf47981444
Akira Hayakawa <ruby.wktk@gmail.com>
2019-03-24 15:18:44 +0900

    dev update
    Signed-off-by: Akira Hayakawa <ruby.wktk@gmail.com>

--- src/bin/ijk-demo.rs
+++ src/bin/ijk-demo.rs
@@ -18,13 +18,31 @@ fn main() {
         .get_matches();
 
     let keys = vec![
-        Char('j'),
-        Char('l'),
+        Char('j'), Char('l'),
         Ctrl('w'),
         Char('\n'),
         Char('h'),
+        // hide/show dot files
+        Char('.'), Char('.'),
+        // open a.rs
+        Char('j'), Char('j'), Char('\n'),
+        Char('/'), Char('x'), Char('\n'),
+        Char('n'), Char('N'), Esc, Char('N'), Esc,
+        Char('w'), Char('w'), Char('w'), Char('b'), Char('b'),
+        // cursor is on number and then change
+        Char('l'), Char('l'), Char('c'), Char('w'), Char('5'), Char('6'), Char('7'), Esc,
+        // undo/redo
+        Char('u'), Ctrl('r'), Char('u'), Ctrl('r'),
+        // jump
+        Char('1'), Char('G'), Char('G'), Char('2'), Char('G'),
+        // move to the latest dir
+        Ctrl('w'), Char('h'),
+        Ctrl('w'), Char('l'),
+        Ctrl('w'), Char('h'),
+        Char('j'), Char('j'), Char('j'), Char('\n'),
+        Ctrl('w'),
     ].into_iter().map(|x| Ok(x));
-    let keys = util::IntervalIterator::new(keys, 1000);
+    let keys = util::IntervalIterator::new(keys, 400);
     
     let navigator = Rc::new(RefCell::new(navigator::Navigator::new()));
 
