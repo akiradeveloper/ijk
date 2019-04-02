faa087184711e8a54574d3378d6c26a7155f200b
Akira Hayakawa <ruby.wktk@gmail.com>
2019-04-02 16:33:37 +0900

    add map function to AsRefMut
    Signed-off-by: Akira Hayakawa <ruby.wktk@gmail.com>

--- src/shared.rs
+++ src/shared.rs
@@ -3,6 +3,9 @@ use std::rc::Rc;
 
 pub trait AsRefMut<T> {
     fn borrow_mut(&self) -> RefMut<T>;
+    fn map<U>(self, f: fn(&mut T) -> &mut U) -> Map<Self,T,U> where Self: Sized {
+        Map::new(self, f)
+    }
 }
 
 impl <T> AsRefMut<T> for Rc<RefCell<T>> {
@@ -11,23 +14,23 @@ impl <T> AsRefMut<T> for Rc<RefCell<T>> {
     }
 }
 
-pub struct Mapped<S,T,U> {
+pub struct Map<S,T,U> {
     orig: S,
     f: fn(&mut T) -> &mut U,
 }
-impl <S,T,U> Mapped<S,T,U> where S: AsRefMut<T> {
+impl <S,T,U> Map<S,T,U> where S: AsRefMut<T> {
     pub fn new(orig: S, f: fn(&mut T) -> &mut U) -> Self {
         Self { orig, f }
     }
 }
-impl <S,T,U> AsRefMut<U> for Mapped<S,T,U> where S: AsRefMut<T> {
+impl <S,T,U> AsRefMut<U> for Map<S,T,U> where S: AsRefMut<T> {
     fn borrow_mut(&self) -> RefMut<U> {
         RefMut::map(self.orig.borrow_mut(), self.f)
     }
 }
-impl <S,T,U> Clone for Mapped<S,T,U> where S: AsRefMut<T> + Clone {
+impl <S,T,U> Clone for Map<S,T,U> where S: AsRefMut<T> + Clone {
     fn clone(&self) -> Self {
-        Mapped::new(self.orig.clone(), self.f)
+        Map::new(self.orig.clone(), self.f)
     }
 }
 
@@ -38,10 +41,12 @@ struct T {
 #[test]
 fn test_shared_mut() {
     let x = Rc::new(RefCell::new(T { x: 0 }));
-    let y0 = Mapped::new(x.clone(), |t| &mut t.x);
-    let y1 = Mapped::new(x.clone(), |t| &mut t.x);
+    let y0 = x.clone().map(|t| &mut t.x);
+    let y1 = x.clone().map(|t| &mut t.x);
     let y2 = y1.clone();
     *y2.borrow_mut() += 10;
+    assert_eq!(x.borrow().x, 10);
     assert_eq!(*y0.borrow_mut(), 10);
     assert_eq!(*y1.borrow_mut(), 10);
+    assert_eq!(*y2.borrow_mut(), 10);
 }
